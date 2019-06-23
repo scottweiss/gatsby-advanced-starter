663e5d174ce63e6589e74a38a9b787cae55f5b9b
Scott Weiss <scott.a.weiss@gmail.com>
2019-06-23 15:48:14 -0700

    make everything pretty

---
+++ src/components/LandingSection/LandingSection.jsx
@@ -0,0 +1,76 @@
+import React from "react";
+import { Link } from "gatsby";
+import ConfettiGenerator from "confetti-js";
+
+
+class LandingSection extends React.Component {
+   constructor(props) {
+      super(props);
+      console.log(props)
+ }
+ 
+
+
+fitToContainer(canvas){
+  // Make it visually fill the positioned parent
+  canvas.style.width ='100%';
+  canvas.style.height='100%';
+  // ...then set the internal size to match
+  canvas.width  = canvas.offsetWidth;
+  canvas.height = canvas.offsetHeight;
+}
+
+componentDidMount() {
+
+	this.renderConfetti();
+	
+	
+}
+renderConfetti() {
+	console.log('render');
+	var id = this.props.id;
+	 
+	var confettiSettings = { target: this.props.id, size: 5, rotate: true, max: 100 };
+	if (this.props.colors != null){
+		confettiSettings.colors =  this.props.colors;
+	}
+	if (this.props.size != null){
+		confettiSettings.size =  this.props.size;
+	} else { confettiSettings.size =  5;}
+	if (this.props.shapes != null){
+		confettiSettings.props =  this.props.shapes;
+	} else {
+		confettiSettings.props =  [ 'square'];
+	}
+	if (this.props.clock != null){
+		confettiSettings.clock =  this.props.clock;
+	} else {
+		confettiSettings.clock =  1;
+	}
+	if (this.props.animate != null){
+		confettiSettings.animate =  this.props.animate;
+	} else {
+		confettiSettings.animate =  false;
+	}
+	var canvas = document.getElementById(this.props.id);
+
+	let confetti = new ConfettiGenerator(confettiSettings);
+	confetti.render();
+}
+  updateDimensions() {
+  var self = this;
+        // this.fitToContainer();
+        console.log('resize');
+        self.renderConfetti;
+    }
+
+  render() {
+    return (
+        <canvas id={this.props.id}>gu</canvas>
+ 
+    );
+  }
+
+}
+
+export default LandingSection;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Helmet from "react-helmet";
 import { graphql } from "gatsby";
 import Layout from "../layout";
+import LandingSection from "../components/LandingSection/LandingSection";
 
 import PostListing from "../components/PostListing/PostListing";
 import SEO from "../components/SEO/SEO";
@@ -15,40 +16,60 @@ class Index extends React.Component {
         <div className="index-container">
           <Helmet title={config.siteTitle} />
           <SEO />
-          <main class="landing ">
-           <header class="landing__header"><h1>Hello World!</h1>
-            <p>Scott Weiss is a Front-End/UI Developer specializing in design systems, accessability, progressive enhancement, responsive design, and style scalability. He currently works for Tembo Inc. in Portland, Oregon.</p>
+          <main className="landing ">
+           <header className="landing__header"><h1>Design Systems-<br /> Accessibility- <br/>   Progressive Enhancement- <br/>Responsive Design-<br/>  Style Architecture-</h1>
+            <p className="large">Scott Weiss is a Front-End/UI Developer (and amature leatherworker) currently based in Portland, Oregon</p>
         
            </header>
+           
+           <section className="landing__intrest">
+            
+             <header>
+               <h2>Portfolio</h2>
+               <p className="m-0">Work that I'm proud of; a mix of professional work and personal projects</p>
+             </header>
+             <LandingSection id="confit-portfolio" animate={false} colors={[[244,120,33]]}></LandingSection>
+           </section>
+                <section className="landing__intrest">
+                  
+             <header>
+               <h2>Blog</h2>
+               <p className="m-0">Discover exciting new CSS, deep dive into my workflow, and intresting challenges I've overcome</p>
+             </header>
+             <LandingSection id="confit-blog" animate={true}></LandingSection>
+           </section>
+                <section className="landing__intrest">
+             <header>
+               <h2>Resume</h2>
+               <p className="m-0">Where I come from and what I've done</p>
+             </header>
+             <LandingSection id="Resume-blog"  animate={false} colors={[[7, 41, 77],[255, 198,0]]}></LandingSection>
+           </section>
+                <section className="landing__intrest" id="landing-experement">
+             <header>
+               <h2>Experiments</h2>
+               <p className="m-0">A place for small micro project and codepen best-ofs</p>
+             </header>
+              <LandingSection id="confit-experiments" animate={true} colors={[[70,184,52], [212,211,211], [43,146,28]]} shapes={["square", "circle"]} clock="-1"></LandingSection>
+           </section>
+                <section className="landing__intrest" id="landing-leather">
+             <header>
+               <h2>Leather Work</h2>
+               <p className="m-0">A journey from complete amature, to a slighlty less amature leatherworker</p>
+             </header>
+              <LandingSection id="confit-leather" animate={false} colors={[[247, 211, 170], [255,119,0]]} shapes={["circle"]}></LandingSection>
+           </section>
+                <section className="landing__intrest">
+             <header>
+               <h2>Contact</h2>
+               <p className="m-0">Send me a message</p>
+             </header>
+              <LandingSection id="confit-contact" animate={false} colors={[[  0, 170, 255]]}></LandingSection>
+           </section>
+
           
           
-          <nav class="landing__nav">
-
-         
-            <a href="" class="landing__article" id="blog-link">
-              <i class="fa-fw fas fa-newspaper"></i>
-              <h2>blog</h2>
-            </a>
-      
-            <a href="" class="landing__article" id="portfolio-link"><i class="fa-fw fas fa-briefcase"></i><h2>portfolio</h2></a>
-            <a class="landing__article" id="leather-link" href="">
-              <i class="fas fa-flask"></i>
-              <h2>experiments</h2>
-            </a>
-            <a class="landing__article" id="leather-link" href="">
-              <i class="fa-fw fas fa-wallet"></i>
-              <h2>leather</h2>
-            </a>
-            <a class="landing__article" id="resume-link" href="/resume">
-              <i class="fa-fw fas fa-file"></i>
-              <h2>resume</h2>
-            </a>
-
-            <a href="/about"class="landing__article" id="contact-link">
-              <i class="fa-fw fas fa-address-card"></i>
-              <h2>contact</h2>
-            </a>
-          </nav>
+       
 
           </main>
           {/* <PostListing postEdges={postEdges} /> */}
