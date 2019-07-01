0cbf96813376edced6e83ae0ca0f43bdd47e5342
Scott Weiss <scott.a.weiss@gmail.com>
2019-07-01 13:09:04 -0700

    update resume section and layout

--- src/pages/resume.jsx
+++ src/pages/resume.jsx
@@ -11,78 +11,141 @@ import config from "../../data/SiteConfig";
 class Resume extends  React.Component {
   render() {
     return (
-    	       <Layout>
+    	<Layout>
         <div className="index-container">
           <Helmet title={config.siteTitle} />
           <SEO />
           <main className="o-page o-page--resume">
             <header className="o-page__header" >
-              <div class="o-page__header-content">
-                  <h1 className="large">See what I can do</h1>
-                  <p className="large">a subset of work</p>
+              	<div className="o-page__header-content">
+                  <h1 className="large">2008 - Now</h1>
+                  <p className="large">a brief overview of time well spent</p>
                 </div>
                 
-                <LandingSection id="confit-blog" max="50"  size="4"  animate={true} clock={'-1'} colors={[[120, 220, 232]]}></LandingSection>
-              </header>
+              <LandingSection class="o-post__header__backdrop o-post__header__backdrop--1"  id="confit-experiments-1" animate={false} size="20" max="5" shapes={["circle"]}  colors={[[ 171, 157, 242], [255, 216, 102], [255, 97, 136]]} clock="-1"></LandingSection>
+              <LandingSection class="o-post__header__backdrop o-post__header__backdrop--2"  max="30" size="2" shapes={["circle"]}  id="confit-experiments-2" colors={[[ 171, 157, 242], [255, 216, 102], [255, 97, 136]]} animate={false}  clock="-1"></LandingSection>
+            </header>
               
          
-       <article class="p-resume">
+      		<article className="p-resume">
+				<section>
+					<article className="p-resume__job">
+						<h1 className="p-resume__employer">Tembo Inc.</h1>
+						<h2 className="p-resume__role">Lead Front End Developer</h2>
+						<p className="p-resume__duration"><i className="p-resume__icon far fa-calendar-alt fa-fw"></i>April 2014 – Present</p>
+						<ul className="p-resume__list">
+							<li>Collaborated with a team of designers and developers to create reporting suites and web tools to allow education departments to quickly analyze and disseminate their data.</li>
+							<li>Developed a scaleable and extendable design system for our primary products; implemented for several state departments of education.</li>
+							<li>Took static design mock-ups and style guides and converted them into adaptive and responsive web components and layouts.</li>
+							<li>Programmatically generated over 80,000 dynamic reports for students in D.C. to assess their standardized test results.</li>
+							<li>Prioritized time, stayed on task, and kept the team updated using JIRA.</li>
+							
+							<li>Worked heavily with Google Maps JavaScript API to display location results, travel times, and distance between multiple entered locations.</li>
+							<li>Utilized ArcGIS data from a government source to enhance a mapping application.</li>
+							<li>Participated in project requirement gathering with stakeholders to ensure clarity across teams and to identify blockers.</li>
+							<li>Implemented functional prototypes with designers for user testing.</li>
+							<li>Troubleshot and confgured network issues offce and served as the main point of contact for our network vendors.</li>
+						</ul>
+					</article >
+
+					<article className="p-resume__job">
+						<h1 className="p-resume__employer">Independence Blue Cross </h1>
+						<h2 className="p-resume__role">Developer - Enterprise Architecture</h2>
+						<p className="p-resume__duration"><i className="p-resume__icon far fa-calendar-alt fa-fw"></i>September 2012 – March 2013</p>
+						<ul className="p-resume__list">
+							<li>Created an application for renaming files for use with the IBX Mobile application.</li>
+							<li>Tested and reported defects for the mobile application on both android and iOS platforms.</li>
+							<li>Designed mock-ups and wireframes for future releases of IBX Mobile.</li>
+						</ul>
+					</article >
+
+					<article className="p-resume__job">
+						<h1 className="p-resume__employer">Cigna</h1>
+						<h2 className="p-resume__role">Production Support - Group Insurance</h2>
+						<p className="p-resume__duration"><i className="p-resume__icon far fa-calendar-alt fa-fw"></i>September 2011 – March 2012</p>
+						<ul className="p-resume__list">
+							<li>Updated and maintained 15 intranet websites using HTML, CSS, .ASP.</li>
+							<li>Gathered requirements from peers and server administration groups.</li>
+							<li>Built a web application utilizing PHP and Ajax for server change tracking.</li>
+							<li>Created a dynamic, easy to use web-based application for sorting servers and attributes.</li>
+							<li>Coordinated the decommission of IVR servers in the Bethlehem offces.</li>
+						</ul>
+					</article>
+					<article className="p-resume__job">
+						<h1 className="p-resume__employer">SAP</h1>
+						<h2 className="p-resume__role">Level 2 Helpdesk Support</h2>
+						<p className="p-resume__duration"><i className="p-resume__icon far fa-calendar-alt fa-fw"></i>September 2010 – March 2011</p>
+						<ul className="p-resume__list">
+							<li>Configured laptops for newly hired employees.</li>
+							<li>Performed data migrations for users receiving new or upgraded laptops.</li>
+							<li>Assisted remote users by troubleshooting problems over the phone.</li>
+							<li>Installed new printers, monitors, and desktops campus wide.</li>
+							<li>Upgraded classrooms with new hardware and cabling.</li>
+						</ul>
+					</article>
+					<article className="p-resume__job">
+						<h1 className="p-resume__employer">Bensalem School District</h1>
+						<h2 className="p-resume__role">Support Technician</h2>
+						<p className="p-resume__duration"><i className="p-resume__icon far fa-calendar-alt fa-fw"></i>2008 - 2009</p>
+						<ul className="p-resume__list">
+							<li>Maintained a series of laptop carts for use in school classrooms.</li>
+							<li>Engaged in troubleshooting networked computers to assure functionality in classroom labs.</li>
+							<li>Relocated and prepared computer set up for staff use.</li>
+							<li>Worked with other tech support members to complete a variety of computer networking projects.</li>
+						</ul>
+					</article>
+			</section>
+
+			<aside className="p-resume__skillset">
+					<section>
+							<h1>Skills</h1>
+					<div className="p-resume__skills" >
+						<ul className="p-resume__list">
+							<lh className="p-resume__list-heading">Essentials</lh>
+							<li>HTML/5</li>
+							<li>CSS/3</li>
+							<li>Flex/Grid</li>
+							<li>Browser limitations</li>
+							<li>Sass</li>
+							<li>Bootstrap</li>
+
+						</ul>
+						<ul className="p-resume__list">
+							<lh className="p-resume__list-heading">Scripting</lh>
+							<li>JavaScript</li>
+							<li>Vue</li>
+							<li>React/GatsbyJS</li>
+							<li>Jekyll/Handlebars/Jinja2</li>
+							<li>Markdown</li>
+							<li>jQuery</li>
+							<li>Gulp/Webpack</li>
+							<li>NPM</li>
+
+						</ul>
+						<ul className="p-resume__list">
+							<lh className="p-resume__list-heading">Other Tools</lh>
+							<li>SQL</li>
+							<li>MySQL</li>
+							<li>Git/Mercurial</li>
+							<li>JIRA/Slack</li>
+							<li>Windows, OSX, Linux</li>
+							<li>Microsoft Offce</li>
+							<li> Google Apps Administration</li>
+						</ul>
+					</div>
+					</section>
+				
+					
+					
 			
-				<section class="c-card">
-					<h1 class="p-resume__position">Tembo Inc.</h1>
-					<span class=" p-resume__position">Lead Front End Developer</span>
-					<p class="p-resume__duration">April 2014 – Present</p>
-					<ul class="p-resume__list">
-						<li>Collaborated with a team of designers and developers to create reporting suites and web tools to allow education departments to quickly analyze and disseminate their data.</li>
-						<li>Took static design mock-ups and style guides and converted them into adaptive and responsive web components.</li>
-						<li>Prioritized time, stayed on task, and kept team updated using JIRA.</li>
-						<li>Programmatically generated over 80,000 dynamic reports for students in D.C. to assess their PARCC test results.</li>
-						<li>Worked heavily with Google Maps JavaScript API to display location results, travel times, and distance between multiple entered locations.</li>
-						<li>Utilized ArcGIS data from a government source to enhance a mapping application.</li>
-						<li>Managed versions across the team using Git.</li>
-						<li>Participated in project requirement gathering with stakeholders to ensure clarity across teams and to identify blockers.</li>
-						<li>Implemented functional prototypes with designers for user testing.</li>
-						<li>Reported to and worked directly with the CIO.</li>
-						<li>Troubleshot and confgured the network for the of ce and served as the main point of contact for network issues.</li>
-					</ul>
-				</section>
 
-				<section class="c-card">
-					<h1 class="p-resume__position">Independence Blue Cross - Enterprise Architecture</h1>
-					<span class=" p-resume__position">Developer</span>
-					<p class="p-resume__duration">September 2012 – March 2013</p>
-					<ul class="p-resume__list">
-						<li>Created an application for renaming files for use with the IBX Mobile application.</li>
-						<li>Tested and reported defects for the mobile application on both android and iOS platforms.</li>
-						<li>Designed mock-ups and wireframes for future releases of IBX Mobile.</li>
-					</ul>
-				</section>
-
-				<section class="c-card">
-					<h1 class="p-resume__position">Cigna - Group Insurance</h1>
-					<span class=" p-resume__position">Production Support</span>
-					<p class="p-resume__duration">September 2011 – March 2012</p>
-					<ul class="p-resume__list">
-						<li>Updated and maintained 15 intranet websites using HTML, CSS, .ASP, and Dreamweaver.</li>
-						<li>Gathered requirements from peers and server administration groups.</li>
-						<li>Built a web application utilizing PHP and Ajax for server change tracking.</li>
-						<li>Created a dynamic, easy to use web-based application for sorting servers and attributes.</li>
-						<li>Coordinated the decommission of IVR servers in the Bethlehem offces.</li>
-					</ul>
-				</section>
-
-				<section class="c-card">
-					<h1>Skills</h1>
-					<p>HTML, CSS3, Sass, JavaScript (jQuery, Ajax, JSON, Gulp), NPM, Bootstrap, SQL, MySQL, Git, Mercurial, JIRA, Windows, OSX, Linux, Microsoft Offce, Google Apps Administration</p>
-				</section>
-
-				<section class="c-card">
+				<section className="">
 					<h1>Education</h1>
 					<p>Bachelor of Science in Information Technology</p>
-					<p class="location">Drexel University, Philadelphia, PA</p>
-					<p class="p-resume__duration">June 2014</p>
+					<p className="location" style={{marginBottom: 0}}>Drexel University,<wbr/> <span style={{whiteSpace: "nowrap"}}>Philadelphia, PA</span></p>
+					<p className="p-resume__duration"><i className="p-resume__icon fas fa-graduation-cap fa-fw"></i>June 2014</p>
 				</section>
-
+	</aside>
 			</article>
           
           
